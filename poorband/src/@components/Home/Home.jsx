66368a9e592fcc46f99ea5c3a18cb293ec803b4e
1wooo <dnjsdn2468@naver.com>
2023-05-22 00:44:09 +0900

    [Fix] Conflict Fix

--- poorband/src/@components/Home/Home.jsx
+++ poorband/src/@components/Home/Home.jsx
@@ -1,13 +1,44 @@
 import React from "react";
 import WriteForm from "./WriteForm";
 import Feed from "./Feed";
+import { styled } from "styled-components";
 
 export default function Home() {
   return (
     <>
-      <div>Home</div>
-      <WriteForm />
-      <Feed />
+      <HomeWrapper>
+        <HomeTag>Home</HomeTag>
+        <WriteForm />
+        <Feed />
+      </HomeWrapper>
     </>
   );
 }
+
+const HomeWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+
+
+  width: 50%;
+  height: 115vh;
+`;
+
+const HomeTag = styled.h2`
+  width: 639px;
+  height: 30px;
+
+
+  margin-bottom: 1vh;
+  margin-left: 1vh;
+
+  width: 639px;
+  height: 30px;
+  
+  font-family: 'Spoqa Han Sans Neo';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 30px;
+`;
