eb25a896f9014005db3ab2842ff3a6f2969f39d9
rninji <yellowminji@naver.com>
2023-05-22 01:29:28 +0900

    [ADD] login

--- poorband/src/App.jsx
+++ poorband/src/App.jsx
@@ -1,18 +1,14 @@
 import { ThemeProvider, styled } from "styled-components";
 import { GlobalStyle } from "./style/globalStyle";
 import { theme } from "./style/theme";
-import Login from "./@pages/Login";
-import Register from "./@pages/Register";
-import Service from "./@pages/Service";
+import Router from "./Router";
 
 export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        {/* <Login />
-        <Register /> */}
-        <Service />
+        <Router />
       </ThemeProvider>
     </>
   );
