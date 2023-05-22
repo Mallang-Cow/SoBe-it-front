dccd66f2da9c17e2ecaf88c641ffd912c1fda22d
parfait2 <parkyl0208@naver.com>
2023-05-22 16:38:44 +0900

    [Feat] SideBar

--- poorband/src/@components/SideBar/SideBar.jsx
+++ poorband/src/@components/SideBar/SideBar.jsx
@@ -2,19 +2,40 @@ import React from "react";
 import SideChallengeCard from "./SideChallengeCard";
 import HotPostCard from "./HotPostCard";
 import SearchBar from "./SearchBar";
+import { styled } from "styled-components";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function SideBar() {
   return (
     <>
-      <div>SideBar</div>
-      <SearchBar />
-      {/* 진행중인 도전과제 있을 경우 */}
-      <SideChallengeCard />
+      <Wrapper>
+        <SearchBar />
+        {/* 진행중인 도전과제 있을 경우 */}
+        <TitleContainer>
+          <h2>Challenge</h2>
+        </TitleContainer>
+        <SideChallengeCard />
 
-      {/* 인기게시글 리스트 - 진행중인 도전과제 있으면 3개, 없으면 5개정도..? */}
-      <HotPostCard />
-      <HotPostCard />
-      <HotPostCard />
+        {/* 인기게시글 리스트 - 진행중인 도전과제 있으면 3개, 없으면 5개정도..? */}
+        <TitleContainer>
+          <h2>Hot Posts</h2>
+        </TitleContainer>
+        <HotPostCard />
+        <HotPostCard />
+        <HotPostCard />
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  background-color: #eee;
+`;
+
+const TitleContainer = styled.div`
+  h2 {
+    text-align: center;
+    font-size: 2rem;
+  }
+`;
