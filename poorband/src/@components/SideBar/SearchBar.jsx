444718fe54066a6646acda269313c80d8a76b5e4
parfait2 <parkyl0208@naver.com>
2023-05-22 16:38:57 +0900

    [Feat] SearchBar

--- poorband/src/@components/SideBar/SearchBar.jsx
+++ poorband/src/@components/SideBar/SearchBar.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { styled } from "styled-components";
 
 export default function SearchBar() {
   return (
     <>
-      <div>SearchBar</div>
-      {/* 누르면 검색 버튼 생기게 */}
+      <Container>
+        <div className="header">
+          <input type="text" className="iptSearch" />
+          <button type="button" className="search">
+            <span>검색</span>
+          </button>
+        </div>
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  .header {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-between;
+    background-color: white;
+  }
+  .iptSearch {
+    width: 100%;
+    border: 0.5rem solid #eee;
+    line-height: 2rem;
+    padding: 0px 1rem;
+    margin-right: 1rem;
+  }
+  button.search {
+    width: 100%;
+    background-color: #eee;
+    padding: 1rem 5rem;
+  }
+`;
