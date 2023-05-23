067f8e5f76ab04af5510d18a2aeaa3da96397250
rninji <yellowminji@naver.com>
2023-05-23 11:29:09 +0900

    [ADD] 달력

--- poorband/src/@components/common/ProgressBar.jsx
+++ poorband/src/@components/common/ProgressBar.jsx
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import { styled } from "styled-components";
 
 export default function ProgressBar(props) {
-  const { baseColor, barColor, percentage } = props;
+  const { basecolor, barcolor, percentage } = props;
   return (
     <>
-      <BaseBar baseColor={baseColor}>
-        <ColorBar barColor={barColor} percentage={percentage}></ColorBar>
+      <BaseBar baseColor={basecolor}>
+        <ColorBar barColor={barcolor} percentage={percentage}></ColorBar>
       </BaseBar>
     </>
   );
 }
 const BaseBar = styled.div`
   border-radius: 5rem;
-  background-color: ${({ baseColor }) => baseColor};
+  background-color: ${({ basecolor }) => basecolor};
   height: 1.2rem;
   width: 100%;
 `;
 const ColorBar = styled.div`
   border-radius: 5rem;
-  background-color: ${({ barColor }) => barColor};
+  background-color: ${({ barcolor }) => barcolor};
   width: ${({ percentage }) => percentage}%;
   height: 1.2rem;
 `;
