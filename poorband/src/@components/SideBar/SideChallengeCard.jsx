341862ad2c1d92b9a3015b7ac54efbd5c1364388
parfait2 <parkyl0208@naver.com>
2023-05-24 23:12:01 +0900

    [Update] sidebar challenge api 연결

--- poorband/src/@components/SideBar/SideChallengeCard.jsx
+++ poorband/src/@components/SideBar/SideChallengeCard.jsx
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import ChallengeProgressBar from "./ChallengeProgressBar";
+import { useQuery } from "react-query";
+import { getChallenge } from "../../../api/challengeAPI";
+import { useMutation } from "react-query";
+import axios from "axios";
+
 export default function SideChallengeCard(props) {
+  const [data, setData] = useState([]);
+  const newData = {
+    userId: "test1",
+  };
+
+  useEffect(() => {
+    challenge(newData);
+  }, []);
+
+  const { mutate: challenge } = useMutation(getChallenge, {
+    onSuccess: (response) => {
+      setData(response.data[response.data.length - 1]); // 사이드바 가장 최근 도전 과제 한 개만 사용.
+    },
+    onError: (error) => {
+      if (error.message === "Request failed with status code 500") {
+        alert("로그인 정보를 다시 확인해 주세요.");
+      }
+    },
+  });
+
   return (
     <Wrapper>
       <TitleWrapper>
-        <p>하루 만원 챌린지🔥🔥🔥</p>
+        <p>{data.title}</p>
       </TitleWrapper>
       <BarWrapper>
         <span>기간</span>
-        <span>23/05/10 - 23/05/31</span>
+        <span>
+          {data.startDate} - {data.endDate}
+        </span>
 
         <ProgressBarWrapper>
           <ProgressBarContainer>
             <ChallengeProgressBar baseColor={"#E7E7E7"} barColor={"#845EC2"} percentage={70}></ChallengeProgressBar>
           </ProgressBarContainer>
-          <p>10,000원</p>
+          <p>{data.goalAmount.toLocaleString()}원</p>
         </ProgressBarWrapper>
       </BarWrapper>
       <RemainWrapper>
         <div id="remain-container">
           <span className="bold">지출</span>
-          <span className="gray">6,900</span>
+          <span className="gray">{data.consumption.toLocaleString()}원</span>
           <span className="bold">잔여</span>
-          <span className="gray">3,100</span>
+          <span className="gray">{(data.goalAmount - data.consumption).toLocaleString()}원</span>
         </div>
       </RemainWrapper>
     </Wrapper>
