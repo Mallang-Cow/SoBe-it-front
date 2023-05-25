a646a9df79ecb5866f732049094cefbd12d8da47
rninji <yellowminji@naver.com>
2023-05-25 14:21:09 +0900

    [ADD] 프로필로 이동

--- poorband/src/@pages/Service.jsx
+++ poorband/src/@pages/Service.jsx
@@ -9,11 +9,16 @@ import Notifications from "../@components/Notifications/Notifications";
 import SearchResults from "../@components/Search/SearchResults";
 import ArticleDetail from "../@components/ArticleDetail/ArticleDetail";
 import ArticleEditForm from "../@components/common/ArticleEditForm";
+import { userIdState } from "../recoil/userId";
+import { useRecoilState } from "recoil";
 
 export default function Service() {
   const [centerContent, setCenterContent] = useState("home");
-  const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useRecoilState(userIdState);
   const [articleSeq, setArticleSeq] = useState(0);
+
+  console.log(userId);
+
   return (
     <>
       <ServiceWrapper>
