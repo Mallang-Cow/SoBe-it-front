8556d9039c798fce90dacd102c63560e40120288
C0012S <alwaysh1002@gmail.com>
2023-05-26 15:57:09 +0900

    [Update] 댓글 알림 데이터 연결

--- poorband/src/@components/Notifications/ReplyNotificationCard.jsx
+++ poorband/src/@components/Notifications/ReplyNotificationCard.jsx
@@ -1,30 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { styled as muiStyled } from '@mui/material/styles';
 import { styled } from "styled-components";
 import { Avatar, IconButton, ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
-import FolderIcon from '@mui/icons-material/Folder';
 import ClearIcon from '@mui/icons-material/Clear';
+import MoodBadIcon from '@mui/icons-material/MoodBad';
 import { theme } from '../../style/theme';
+// import { SIDEBAR_DETAIL } from "../../../core/sideBarData"; //"../../core/sideBarData";
 
-export default function ReplyNotificationCard() {
+export default function ReplyNotificationCard({ type, content, articleContent, url, imageUrl, timestamp }) {
+  const [time, setTime] = useState([]);
+  const nowDate = new Date();
+  const navigate = useNavigate();
+
+  // 댓글이 작성된 글로 이동
+  const navigateToArticle = () => {
+    console.log({ url });
+    // const currentOrigin = window.location.origin;
+    // console.log(currentOrigin);
+    // const relativeUrl = url.replace('currentOrigin', ''); // 도메인과 포트 번호 제거
+    // console.log(relativeUrl);
+    // navigate(relativeUrl);
+
+    // setCenterContent() 방식으로 해야 됨
+  };
+
+  // // 상세 페이지로 이동
+  // function goToArticleDetail() {
+    // setArticleSeq(thisArticleSeq);
+    // setCenterContent("detail");
+  //   }
+  // }
+
+  // 사용자 프로필 이미지
+  let avatarImg = null;
+  if (imageUrl) {
+    avatarImg = <img src={ imageUrl } alt="사용자 프로필 이미지" style={{ width: "100%", height: "100%" }} />;
+  }
+  else {
+//    avatarImg = <img src={ SIDEBAR_DETAIL.user.profileImageUrl } style={{ width: "100%", height: "100%", borderRadius: "1rem" }} />;
+    avatarImg = <MoodBadIcon style={{ width: "6rem", height: "6rem" }}/>;
+  }
+
+  // 알림 시간 구하기
+  useEffect(() => {
+    const notificationDate = new Date(timestamp);
+    if (notificationDate.getFullYear() != nowDate.getFullYear())
+      setTime(["year", nowDate.getFullYear() - notificationDate.getFullYear()]); // 연도 차이
+    if (notificationDate.getMonth() != nowDate.getMonth())
+      setTime(["month", nowDate.getMonth() - notificationDate.getMonth()]); // 달 차이
+    else if (notificationDate.getDate() != nowDate.getDate())
+      setTime(["date", nowDate.getDate() - notificationDate.getDate()]); // 일 차이
+    else if (notificationDate.getHours() != nowDate.getHours())
+      setTime(["hours", nowDate.getHours() - notificationDate.getHours()]); // 시간 차이
+    else if (notificationDate.getMinutes() != nowDate.getMinutes())
+      setTime(["minutes", nowDate.getMinutes() - notificationDate.getMinutes()]); // 분 차이
+    else setTime(["seconds", nowDate.getSeconds() - notificationDate.getSeconds()]); // 초 차이
+  }, []);
+
+  const deleteNotification = (event) => {
+    event.stopPropagation(); // 이벤트 버블링 중단
+    console.log("삭제 버튼 클릭");
+  };
+
   return (
-    <NotificationCardButton disableRipple>
+    <NotificationCardButton onClick={ navigateToArticle } disableRipple>
       <ListItem secondaryAction={
-                    <NotificationIconButton edge="end" aria-label="delete">
-                      <ClearIcon />
-                    </NotificationIconButton> }>
-        
+        <NotificationIconButton edge="end" aria-label="delete" onClick={ deleteNotification } disableRipple>
+          <ClearIcon />
+        </NotificationIconButton>
+      }>
         <ListItemAvatar style={{ width: "6rem", height: "6rem" }}>
           <Avatar style={{ width: "6rem", height: "6rem" }}>
-            <FolderIcon />
+            { avatarImg }
           </Avatar>
         </ListItemAvatar>
         <div style={{ marginLeft: "2rem" }}>
           <div style={{ display: "flex", flexDirection: "row", marginBottom: '0.6rem', alignItems: 'center' }}>
-            <MainNotificationText primary="Reply Content" />
-            <TimeNotificationText primary="time" />
+            <MainNotificationText primary={ content } />
+            <TimeNotificationText primary={ time && (
+              <p className="grey">
+                {time[1]}
+                {time[0] === "year" && "년"}
+                {time[0] === "month" && "월"}
+                {time[0] === "date" && "일"}
+                {time[0] === "hours" && "시간"}
+                {time[0] === "minutes" && "분"}
+                {time[0] === "secounds" && "초"} 전
+              </p>
+            )} />
           </div>
-          <SubNotificationText secondary="Article Content" />
+          <SubNotificationText secondary={ articleContent } />
         </div>
       </ListItem>
     </NotificationCardButton>
@@ -40,10 +106,6 @@ const NotificationCardButton = muiStyled(ListItemButton) ({
   },
   '%:focus': {
     backgroundColor: 'none',
-  },
-  '& .MuiButtonBase-root': {
-    // ListItemButton 내의 ButtonBase 컴포넌트에 대한 스타일링
-    pointerEvents: 'none',
   },
 });
 
