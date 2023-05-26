371fbb2c2d271f679bad8415a1bfa019cc434bb8
C0012S <alwaysh1002@gmail.com>
2023-05-26 09:51:52 +0900

    [Feat] 로그아웃 기능 추가

--- poorband/src/@components/MenuBar.jsx
+++ poorband/src/@components/MenuBar.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "react-query";
 import { styled } from "styled-components";
+import { signout } from "../../api/userAPI";
+import { ACCESS_TOKEN } from '../../api/ApiService';
 import { SIDEBAR_DETAIL } from "../../core/sideBarData";
 
 export default function MenuBar(props) {
   const { centerContent, setCenterContent, setUserId } = props;
+  const navigate = useNavigate();
+
+  const { mutate: logoutUser } = useMutation(signout, {
+    onSuccess: (response) => {
+      console.log(response);
+      sessionStorage.setItem(ACCESS_TOKEN, null);
+      navigate("/"); // 추후 경로 /login으로 수정
+    },
+    onError: (error) => {
+      if (error.message === "Request failed with status code 500") {
+        console.log(error.response.data.error);
+        alert("로그아웃 하는 과정에 오류가 발생했습니다.");
+      }
+    },
+  });
+
+  const logout = async () => {
+    logoutUser();
+  };
 
   return (
     <Wrapper>
@@ -83,7 +105,7 @@ export default function MenuBar(props) {
         </ProfileWrapper>
         <LogoutWrapper>
           <span className="material-symbols-outlined">logout</span>
-          <button>로그아웃</button>
+          <button onClick={ logout }>로그아웃</button>
         </LogoutWrapper>
       </BottomWrapper>
     </Wrapper>
