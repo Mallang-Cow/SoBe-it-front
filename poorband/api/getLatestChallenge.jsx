ecc1c717c7998ff961c93a0ea2e11c5889060bf2
parfait2 <parkyl0208@naver.com>
2023-05-28 02:30:30 +0900

    [Fix] 사이드바 도전 과제 API 수정 및 오류 해결

---
+++ poorband/api/getLatestChallenge.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+export async function getLatestChallenge(newData) {
+  const data = await axios.post(`http://localhost:9000/profile/challenge/latest`, newData, {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${window.sessionStorage.getItem("ACCESS_TOKEN")}`,
+    },
+  });
+  return data;
+}
