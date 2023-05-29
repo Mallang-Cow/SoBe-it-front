e25629ea8a078d4d517e470b04a8fc171b853726
rninji <yellowminji@naver.com>
2023-05-29 21:14:14 +0900

    [ADD] 이미지

--- poorband/src/@pages/FindId.jsx
+++ poorband/src/@pages/FindId.jsx
@@ -2,10 +2,10 @@ import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "react-query";
 import { findId } from "../../api/userAPI";
-import { Button, ButtonGroup, Container, TextField, Typography } from '@mui/material';
-import { createTheme, styled as muiStyled, ThemeProvider } from '@mui/material/styles';
-import { theme } from '../style/theme';
-import '../style/RootContainer.css';
+import { Button, ButtonGroup, Container, TextField, Typography } from "@mui/material";
+import { createTheme, styled as muiStyled, ThemeProvider } from "@mui/material/styles";
+import { theme } from "../style/theme";
+import "../style/RootContainer.css";
 import { SIDEBAR_DETAIL } from "../../core/sideBarData";
 
 export default function FindId() {
@@ -27,11 +27,11 @@ export default function FindId() {
   const handleFindId = async () => {
     const findIdDTO = {
       inputUserName: userNameRef.current.value,
-      inputUserPhoneNumber: userPhoneNumberRef.current.value
+      inputUserPhoneNumber: userPhoneNumberRef.current.value,
     };
 
     findUserId(findIdDTO);
-  }
+  };
 
   const navigateToRegister = () => {
     navigate("/register");
@@ -47,25 +47,27 @@ export default function FindId() {
 
   return (
     <div className="RootContainer">
-      <Container component="main" style={{ width:"54rem" }}>
+      <Container component="main" style={{ width: "54rem" }}>
         <LogoContainer>
-          <img src={SIDEBAR_DETAIL.user.profileImageUrl} alt="로고" style={{ width: "3rem", height: "3rem", borderRadius: "1rem" }}/>
+          <img src="https://i.ibb.co/J742sC7/1-5.png" alt="로고" style={{ width: "6rem", marginBottom: "2rem" }} />
         </LogoContainer>
 
         <TitleContainer>
           <TitleTypography variant="h3">아이디 찾기</TitleTypography>
         </TitleContainer>
 
-        <FormContainer >
-          <InputTextField label="이름" type="text" fullWidth inputRef={ userNameRef }/>
-          <InputTextField label="전화번호" type="text" fullWidth inputRef={ userPhoneNumberRef } />
-          <FindIdButton variant="contained" fullWidth onClick={ handleFindId }>아이디 찾기</FindIdButton>
+        <FormContainer>
+          <InputTextField label="이름" type="text" fullWidth inputRef={userNameRef} />
+          <InputTextField label="전화번호" type="text" fullWidth inputRef={userPhoneNumberRef} />
+          <FindIdButton variant="contained" fullWidth onClick={handleFindId}>
+            아이디 찾기
+          </FindIdButton>
 
-          <ThemeProvider theme={ ButtonGroupTheme } >
+          <ThemeProvider theme={ButtonGroupTheme}>
             <ButtonGroup variant="text" aria-label="text button group" color="primary" fullWidth>
-              <GroupButton onClick={ navigateToRegister }>회원가입</GroupButton>
-              <GroupButton onClick={ navigateToLogin }>로그인</GroupButton>
-              <GroupButton onClick={ navigateToFindPassword }>비밀번호 찾기</GroupButton>
+              <GroupButton onClick={navigateToRegister}>회원가입</GroupButton>
+              <GroupButton onClick={navigateToLogin}>로그인</GroupButton>
+              <GroupButton onClick={navigateToFindPassword}>비밀번호 찾기</GroupButton>
             </ButtonGroup>
           </ThemeProvider>
         </FormContainer>
@@ -74,89 +76,81 @@ export default function FindId() {
   );
 }
 
-const LogoContainer = muiStyled(Container) ({
+const LogoContainer = muiStyled(Container)({});
 
+const TitleContainer = muiStyled(Container)({
+  paddingBottom: "2.2rem",
 });
 
-const TitleContainer = muiStyled(Container) ({
-  paddingBottom: '2.2rem',
-});
-
-const TitleTypography = muiStyled(Typography) ({
-  fontSize: '4.2rem',
-  fontFamily: [
-    'Spoqa Han Sans Neo',
-  ].join(','),
-  fontStyle: 'normal',
+const TitleTypography = muiStyled(Typography)({
+  fontSize: "4.2rem",
+  fontFamily: ["Spoqa Han Sans Neo"].join(","),
+  fontStyle: "normal",
   fontWeight: 700,
-  letterSpacing: '0.03em',
+  letterSpacing: "0.03em",
 });
 
-const FormContainer = muiStyled(Container) ({
+const FormContainer = muiStyled(Container)({});
 
-});
-
-const InputTextField = muiStyled(TextField) ({
-  marginBottom: '2rem',
-  '& input': {
-    height: '1.8rem',
-    fontSize: '1.6rem',
+const InputTextField = muiStyled(TextField)({
+  marginBottom: "2rem",
+  "& input": {
+    height: "1.8rem",
+    fontSize: "1.6rem",
   },
-  '& label': {
-    fontSize: '1.6rem',
+  "& label": {
+    fontSize: "1.6rem",
     color: theme.colors.lightgrey_2, // 기본 label 색상
   },
-  '& label.Mui-focused': {
-    fontSize: '1.6rem',
+  "& label.Mui-focused": {
+    fontSize: "1.6rem",
     color: theme.colors.darkgrey_1,
   },
-  '& .MuiInput-underline:after': {
+  "& .MuiInput-underline:after": {
     borderBottomColor: theme.colors.lightpurple,
   },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
       borderColor: theme.colors.lightgrey_2,
     },
-    '&:hover fieldset': {
+    "&:hover fieldset": {
       borderColor: theme.colors.darkgrey_1,
     },
-    '&.Mui-focused fieldset': {
+    "&.Mui-focused fieldset": {
       borderColor: theme.colors.lightpurple,
     },
   },
 });
 
-const FindIdButton = muiStyled(Button) ({
+const FindIdButton = muiStyled(Button)({
   color: theme.colors.white,
-  height: '4.6rem',
-  marginTop: '1rem',
-  marginBottom: '1.8rem',
-  textTransform: 'none',
-  fontSize: '1.8rem',
-  fontFamily: [
-    'Spoqa Han Sans Neo',
-  ].join(','),
-  fontStyle: 'normal',
+  height: "4.6rem",
+  marginTop: "1rem",
+  marginBottom: "1.8rem",
+  textTransform: "none",
+  fontSize: "1.8rem",
+  fontFamily: ["Spoqa Han Sans Neo"].join(","),
+  fontStyle: "normal",
   fontWeight: 700,
-  letterSpacing: '0.03em',
+  letterSpacing: "0.03em",
   backgroundColor: theme.colors.mainpurple,
   borderColor: theme.colors.lightgrey_2,
-  '&:hover': {
-    backgroundColor: theme.colors.darkpurple_2
+  "&:hover": {
+    backgroundColor: theme.colors.darkpurple_2,
   },
-  '&:active': {
-    boxShadow: 'none',
+  "&:active": {
+    boxShadow: "none",
     backgroundColor: theme.colors.lightpurple,
     borderColor: theme.colors.lightgrey_1,
   },
-  '&:focus': {
-    boxShadow: '0 0 0 0.04rem #EDEDED',
-    border: 'none',
-    outline: 'none',
+  "&:focus": {
+    boxShadow: "0 0 0 0.04rem #EDEDED",
+    border: "none",
+    outline: "none",
   },
 });
 
-const ButtonGroupTheme = createTheme ({
+const ButtonGroupTheme = createTheme({
   palette: {
     primary: {
       main: theme.colors.mainpurple,
@@ -164,19 +158,17 @@ const ButtonGroupTheme = createTheme ({
   },
 });
 
-const GroupButton = muiStyled(Button) ({
+const GroupButton = muiStyled(Button)({
   color: theme.colors.mainpurple,
-  fontSize: '1.6rem',
-  fontFamily: [
-    'Spoqa Han Sans Neo',
-  ].join(','),
-  fontStyle: 'normal',
+  fontSize: "1.6rem",
+  fontFamily: ["Spoqa Han Sans Neo"].join(","),
+  fontStyle: "normal",
   fontWeight: 400,
-  letterSpacing: '0.03em',  
-  textTransform: 'none',
-  '&:focus': {
-    boxShadow: '0 0 0 0.04rem #EDEDED',
-    border: 'none',
-    outline: 'none',
+  letterSpacing: "0.03em",
+  textTransform: "none",
+  "&:focus": {
+    boxShadow: "0 0 0 0.04rem #EDEDED",
+    border: "none",
+    outline: "none",
   },
 });
--- poorband/src/@pages/Login.jsx
+++ poorband/src/@pages/Login.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "react-query";
 import { signin } from "../../api/userAPI";
-import { Button, ButtonGroup, Container, TextField, Typography } from '@mui/material';
-import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
-import { theme } from '../style/theme';
-import '../style/RootContainer.css';
+import { Button, ButtonGroup, Container, TextField, Typography } from "@mui/material";
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import { theme } from "../style/theme";
+import "../style/RootContainer.css";
 import { SIDEBAR_DETAIL } from "../../core/sideBarData";
 
 export default function Login() {
@@ -18,8 +18,7 @@ export default function Login() {
 
     if (token && token !== "null" && token !== "") {
       return true; // 토큰이 null과 빈 문자열이 아닌 다른 값으로 존재할 경우 true 반환
-    }
-    else {
+    } else {
       return false; // 토큰이 null이고 빈 문자열일 경우 false 반환
     }
   };
@@ -68,25 +67,27 @@ export default function Login() {
 
   return (
     <div className="RootContainer">
-      <Container component="main" style={{ width:"54rem" }}>
+      <Container component="main" style={{ width: "54rem" }}>
         <LogoContainer>
-          <img src={SIDEBAR_DETAIL.user.profileImageUrl} alt="로고" style={{ width: "3rem", height: "3rem", borderRadius: "1rem" }}/>
+          <img src="https://i.ibb.co/J742sC7/1-5.png" alt="로고" style={{ width: "6rem", marginBottom: "2rem" }} />
         </LogoContainer>
 
         <TitleContainer>
           <TitleTypography variant="h3">Log in</TitleTypography>
         </TitleContainer>
 
-        <FormContainer >
-          <InputTextField label="ID" type="text" fullWidth inputRef={ idRef }/>
-          <InputTextField label="Password" type="password" fullWidth inputRef={ passwordRef } />
-          <LoginButton variant="contained" fullWidth onClick={ login }>Log In</LoginButton>
+        <FormContainer>
+          <InputTextField label="ID" type="text" fullWidth inputRef={idRef} />
+          <InputTextField label="Password" type="password" fullWidth inputRef={passwordRef} />
+          <LoginButton variant="contained" fullWidth onClick={login}>
+            Log In
+          </LoginButton>
 
-          <ThemeProvider theme={ ButtonGroupTheme } >
+          <ThemeProvider theme={ButtonGroupTheme}>
             <ButtonGroup variant="text" aria-label="text button group" color="primary" fullWidth>
-              <GroupButton onClick={ navigateToRegister }>회원가입</GroupButton>
-              <GroupButton onClick={ navigateToFindId }>아이디 찾기</GroupButton>
-              <GroupButton onClick={ navigateToFindPassword }>비밀번호 찾기</GroupButton>
+              <GroupButton onClick={navigateToRegister}>회원가입</GroupButton>
+              <GroupButton onClick={navigateToFindId}>아이디 찾기</GroupButton>
+              <GroupButton onClick={navigateToFindPassword}>비밀번호 찾기</GroupButton>
             </ButtonGroup>
           </ThemeProvider>
         </FormContainer>
@@ -95,89 +96,81 @@ export default function Login() {
   );
 }
 
-const LogoContainer = styled(Container) ({
+const LogoContainer = styled(Container)({});
 
+const TitleContainer = styled(Container)({
+  paddingBottom: "2.2rem",
 });
 
-const TitleContainer = styled(Container) ({
-  paddingBottom: '2.2rem',
-});
-
-const TitleTypography = styled(Typography) ({
-  fontSize: '4.2rem',
-  fontFamily: [
-    'Spoqa Han Sans Neo',
-  ].join(','),
-  fontStyle: 'normal',
+const TitleTypography = styled(Typography)({
+  fontSize: "4.2rem",
+  fontFamily: ["Spoqa Han Sans Neo"].join(","),
+  fontStyle: "normal",
   fontWeight: 700,
-  letterSpacing: '0.03em',
+  letterSpacing: "0.03em",
 });
 
-const FormContainer = styled(Container) ({
+const FormContainer = styled(Container)({});
 
-});
-
-const InputTextField = styled(TextField) ({
-  marginBottom: '2rem',
-  '& input': {
-    height: '1.8rem',
-    fontSize: '1.6rem',
+const InputTextField = styled(TextField)({
+  marginBottom: "2rem",
+  "& input": {
+    height: "1.8rem",
+    fontSize: "1.6rem",
   },
-  '& label': {
-    fontSize: '1.6rem',
+  "& label": {
+    fontSize: "1.6rem",
     color: theme.colors.lightgrey_2, // 기본 label 색상
   },
-  '& label.Mui-focused': {
-    fontSize: '1.6rem',
+  "& label.Mui-focused": {
+    fontSize: "1.6rem",
     color: theme.colors.darkgrey_1,
   },
-  '& .MuiInput-underline:after': {
+  "& .MuiInput-underline:after": {
     borderBottomColor: theme.colors.lightpurple,
   },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
       borderColor: theme.colors.lightgrey_2,
     },
-    '&:hover fieldset': {
+    "&:hover fieldset": {
       borderColor: theme.colors.darkgrey_1,
     },
-    '&.Mui-focused fieldset': {
+    "&.Mui-focused fieldset": {
       borderColor: theme.colors.lightpurple,
     },
   },
 });
 
-const LoginButton = styled(Button) ({
+const LoginButton = styled(Button)({
   color: theme.colors.white,
-  height: '4.6rem',
-  marginTop: '1rem',
-  marginBottom: '1.8rem',
-  textTransform: 'none',
-  fontSize: '1.8rem',
-  fontFamily: [
-    'Spoqa Han Sans Neo',
-  ].join(','),
-  fontStyle: 'normal',
+  height: "4.6rem",
+  marginTop: "1rem",
+  marginBottom: "1.8rem",
+  textTransform: "none",
+  fontSize: "1.8rem",
+  fontFamily: ["Spoqa Han Sans Neo"].join(","),
+  fontStyle: "normal",
   fontWeight: 700,
-  letterSpacing: '0.03em',
+  letterSpacing: "0.03em",
   backgroundColor: theme.colors.mainpurple,
   borderColor: theme.colors.lightgrey_2,
-  '&:hover': {
-    backgroundColor: theme.colors.darkpurple_2
+  "&:hover": {
+    backgroundColor: theme.colors.darkpurple_2,
   },
-  '&:active': {
-    boxShadow: 'none',
+  "&:active": {
+    boxShadow: "none",
     backgroundColor: theme.colors.lightpurple,
     borderColor: theme.colors.lightgrey_1,
   },
-  '&:focus': {
-    boxShadow: '0 0 0 0.04rem #EDEDED',
-    border: 'none',
-    outline: 'none',
+  "&:focus": {
+    boxShadow: "0 0 0 0.04rem #EDEDED",
+    border: "none",
+    outline: "none",
   },
 });
 
-const ButtonGroupTheme = createTheme ({
+const ButtonGroupTheme = createTheme({
   palette: {
     primary: {
       main: theme.colors.mainpurple,
@@ -185,19 +178,17 @@ const ButtonGroupTheme = createTheme ({
   },
 });
 
-const GroupButton = styled(Button) ({
+const GroupButton = styled(Button)({
   color: theme.colors.mainpurple,
-  fontSize: '1.6rem',
-  fontFamily: [
-    'Spoqa Han Sans Neo',
-  ].join(','),
-  fontStyle: 'normal',
+  fontSize: "1.6rem",
+  fontFamily: ["Spoqa Han Sans Neo"].join(","),
+  fontStyle: "normal",
   fontWeight: 400,
-  letterSpacing: '0.03em',  
-  textTransform: 'none',
-  '&:focus': {
-    boxShadow: '0 0 0 0.04rem #EDEDED',
-    border: 'none',
-    outline: 'none',
+  letterSpacing: "0.03em",
+  textTransform: "none",
+  "&:focus": {
+    boxShadow: "0 0 0 0.04rem #EDEDED",
+    border: "none",
+    outline: "none",
   },
 });
